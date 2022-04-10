f98f4c189a1d4812fb8ac9f5af0db6306029ba93
fshaaa <fathshaffa@gmail.com>
2022-04-10 14:18:30 +0700

    fix soal1.R

--- soal1.R
+++ soal1.R
@@ -12,6 +12,23 @@ mean(rgeom(10000, 0.2) == 3)
 #ternyata hasilnya untuk nilai b hampir mendekati nilai exact
 
 #d
+library(dplyr)
+library(ggplot2)
+
+data.frame(x = 0:10, prob = dgeom(x = 0:10, prob = 0.2)) %>%
+  mutate(Failures = ifelse(x == 3, 3, "other")) %>%
+  ggplot(aes(x = factor(x), y = prob, fill = Failures)) +
+  geom_col() +
+  geom_text(
+    aes(label = round(prob,2), y = prob + 0.01),
+    position = position_dodge(0.9),
+    size = 3,
+    vjust = 0
+  ) +
+  labs(title = "Probability of X = 3 Failures Prior to First Success",
+       subtitle = "Geometric(.2)",
+       x = "Failures prior to first success (x)",
+       y = "Probability")
 
 #e
 rataan <- 1/0.2 #1/p
